5a54dfbd4b844b2fcd97f687aea4cfcf20876de2
jseffker <jseffker@gmail.com>
2023-01-19 14:10:13 -0700

    1/19

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,22 +1,31 @@
 import { FormEvent, useEffect, useState } from 'react'
 
 async function getQuotes(){
-    const result = await fetch("https://api.quotable.io/search/quotes?query=George&fields=author");
+    //const result = await fetch("https://api.quotable.io/search/quotes?query=George&fields=author");
+    const result = await fetch("https://quotable.io/quotes?author=albert-einstein");
     //console.log(await result.json());
     let quotes = await result.json();
     return quotes;
 
 }
 
+interface Quote {
+    author: string;
+    quote: string;
+}
+
 
 export const SearchBar = function() {
+    const [quoteList, setQuoteList] = useState<Quote[]>([]);
     const search = async function(event: FormEvent) {
         event.preventDefault()
-        useEffect(() => {
-            getQuotes().then(response => {
-                console.log("Hi "+ Object.keys(response));
-            });
-        }, []);
+        getQuotes().then(response => {
+            console.log("Hi "+ Object.keys(response));
+            for (const item of response.results)
+            {
+                console.log("Hi "+ item.content);
+            }
+        });
     }
     
     return (
